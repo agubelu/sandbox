31b8dcc88a752088730b1ffc11ba9e215d925106
agubelu <agustinbd3@gmail.com>
2024-10-06 11:47:29 +0200

    Fix water

--- src/particle.rs
+++ src/particle.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use wasm_bindgen::prelude::*;
 
 use crate::{Color, Sandbox, kind_matches};
@@ -64,23 +65,32 @@ impl Particle {
     }
 
     fn update_water(&mut self, pos: usize, world: &Sandbox) -> Option<usize> {
-        // Sand tries to fall down, down-left and down-right.
-        // It can go through emptiness or water.
         let down = pos + world.width();
         let down_left = down - 1;
         let down_right = down + 1;
+        let left = pos - 1;
+        let right = pos + 1;
 
-        if world.is_bottom_row(pos) {
-            return None;
+        let is_left_col = world.is_left_col(pos);
+        let is_right_col = world.is_right_col(pos);
+
+        if !world.is_bottom_row(pos) {
+            if kind_matches!(world, down, Empty) {
+                return Some(down);
+            } else if !is_left_col && kind_matches!(world, down_left, Empty) {
+                return Some(down_left);
+            } else if !is_right_col && kind_matches!(world, down_right, Empty) {
+                return Some(down_right);
+            }
         }
 
-        if kind_matches!(world, down, Empty) {
-            return Some(down);
-        } else if !world.is_left_col(pos) && kind_matches!(world, down_left, Empty) {
-            return Some(down_left);
-        } else if !world.is_right_col(pos) && kind_matches!(world, down_right, Empty) {
-            return Some(down_right);
+        if !is_left_col && rand::thread_rng().gen::<f32>() < 0.5 && kind_matches!(world, left, Empty) {
+            return Some(left);
+        } else if !is_right_col && rand::thread_rng().gen::<f32>() < 0.5 && kind_matches!(world, right, Empty) {
+            return Some(right);
         }
         None
     }
 }
+
+
